19dc2831c5533c21c72e00cbdedc6225536aeab5
Jan Schneider <jan@horde.org>
2013-02-19 17:21:01 +0100

    Be PHP 5.2 compatible.

--- File.php
+++ File.php
@@ -4,20 +4,20 @@ class File
 {
   public function __construct()
   {
-    mkdir(__DIR__ . '/test');
-    touch(__DIR__ . '/test/高&执&行&力&的&打&造.txt');
+    mkdir(dirname(__FILE__) . '/test');
+    touch(dirname(__FILE__) . '/test/高&执&行&力&的&打&造.txt');
   }
 
   public function __destruct()
   {
-    unlink(__DIR__ . '/test/高&执&行&力&的&打&造.txt');
-    rmdir(__DIR__ . '/test');
+    unlink(dirname(__FILE__) . '/test/高&执&行&力&的&打&造.txt');
+    rmdir(dirname(__FILE__) . '/test');
   }
 
   public function listFiles()
   {
     $files = array();
-    $d = dir(__DIR__ . '/test');
+    $d = dir(dirname(__FILE__) . '/test');
     while (($entry = $d->read()) !== false) {
       if ($entry == '.' || $entry == '..') {
         continue;
